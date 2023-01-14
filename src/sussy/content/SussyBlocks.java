2f91be8c9058a89affaa5f06b75e1f10da15aea5
Jestyak2009 <118631437+Jestyak2009@users.noreply.github.com>
2023-01-14 02:43:01 +0200

    Sussy walls

---
+++ src/sussy/content/SussyBlocks.java
@@ -0,0 +1,65 @@
+package sussy.content;
+
+import arc.graphics.*;
+import arc.math.*;
+import arc.struct.*;
+import mindustry.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.DrawPart.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.unit.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.heat.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.payloads.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+import static mindustry.type.ItemStack.*;
+
+public class SussyBlocks{
+    public static Block
+    
+    //defense
+    SussyWall, SussyWallLarge,
+    ;
+    
+    public static void load(){
+        SussyWall = new Wall("SussyWall"){{
+            requirements(Category.defense, with(Items.copper, 50));
+            health = 450 * wallHealthMultiplier;
+            armor = 3f;
+            buildCostMultiplier = 8f;
+        }};
+      
+        SussyWallLarge = new Wall("SussyWallLarge"){{
+            requirements(Category.defense, with(Items.copper, 100));
+            health = 850 * wallHealthMultiplier;
+            armor = 10f;
+            buildCostMultiplier = 5f;
+            size = 2;
+        }};
+    }
+}
