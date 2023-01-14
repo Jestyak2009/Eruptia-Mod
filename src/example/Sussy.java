2d6c9c0d0c70e42fc4240f81c47ba3cfb9f4d5b1
Jestyak2009 <118631437+Jestyak2009@users.noreply.github.com>
2023-01-14 02:11:40 +0200

    Suuuuus

--- src/example/Sussy.java
+++ src/example/Sussy.java
@@ -1,4 +1,4 @@
-package example;
+package sussy;
 
 import arc.*;
 import arc.util.*;
@@ -9,9 +9,9 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class Sussy extends Mod{
 
-    public ExampleJavaMod(){
+    public Sussy(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
@@ -19,10 +19,10 @@ public class ExampleJavaMod extends Mod{
             //show dialog upon startup
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
+                dialog.cont.add("When imposter is sus").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.cont.button("Tuturutututuru", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
@@ -30,7 +30,7 @@ public class ExampleJavaMod extends Mod{
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("Loading some sussy content.");
     }
 
 }
