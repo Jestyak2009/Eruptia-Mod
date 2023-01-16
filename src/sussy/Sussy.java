506288a8d36aa7e9be9bd3f11492df885d4b4477
Prosta4ok_ua <danyavdele@gmail.com>
2023-01-16 22:00:41 +0200

    fix imports

--- src/sussy/Sussy.java
+++ src/sussy/Sussy.java
@@ -8,6 +8,7 @@ import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
+import sussy.content.SussyBlocks;
 
 public class Sussy extends Mod{
 
